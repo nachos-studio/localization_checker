a5ef6475fbc4180f47cc5320c8efb180641cce96
Armine Balian <balian.armine@gmail.com>
2022-12-06 14:11:25 +0300

    PR-101: Increase version number
    New version - 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="localization-checker",
-    version="0.0.3",
+    version="0.0.7",
     author="Armine Romanyuta",
     author_email="romanyuta.armine@gmail.com",
     description="Package to compare and complete all languages in your project with main language",
